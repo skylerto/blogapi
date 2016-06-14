7dd4921b82ee0bf25882f67aacaff571e0b2a1b7
Skyler Layne <skylerclayne@gmail.com>
2016-06-14 09:30:52 -0400

    Add post data now in an array.
    Post data to /blog/posts/new now gets added to an array, visable from
    /blog/posts

--- src/main/java/me/skylerlayne/dev/main/MainController.java
+++ src/main/java/me/skylerlayne/dev/main/MainController.java
@@ -4,38 +4,60 @@ package me.skylerlayne.dev.main;
 import me.skylerlayne.dev.bean.PostBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 /**
  * This class represents the main controller for the API.
+ * <p>
+
+ * <p>
  * <p>
  * Created by Skyler Layne © 2016
  */
 @RestController
 public class MainController {
 
-    @RequestMapping(method= RequestMethod.GET, value="/test")
-    public @ResponseBody
+    @RequestMapping(method = RequestMethod.GET, value = "/test")
+    public
+    @ResponseBody
     Test getTest() {
         return new Test("BlogAPI", "This is a blog API written in Spring!");
     }
 
 
-
-    @RequestMapping(method= RequestMethod.GET, value="/test-route")
-    public @ResponseBody
-    PostBean newPost(@RequestParam(value="title", required=false, defaultValue="Post Title") String title, @RequestParam(value="content", required=false, defaultValue="Post Content") String content) {
-
-        ApplicationContext context = new ClassPathXmlApplicationContext(
-                "Spring-Module.xml");
-
-
-
+    @RequestMapping(method = RequestMethod.GET, value = "/test-route")
+    public
+    @ResponseBody
+    PostBean testPost(@RequestParam(value = "title", required = false, defaultValue = "Post Title") String title, @RequestParam(value = "content", required = false, defaultValue = "Post Content") String content) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Module.xml");
         PostBean obj = (PostBean) context.getBean("postBean");
         System.out.print(obj.getTitle() + ": " + obj.getContent());
-
         return new PostBean(title, content);
+    }
+
+
+
+
+    /* BLOG API */
+
+    ArrayList<PostBean> posts = new ArrayList<>();
+
+    @RequestMapping(method = RequestMethod.GET, value = "/blog/posts")
+    public
+    @ResponseBody
+    ArrayList<PostBean> getPosts() {
+        return posts;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/blog/posts/new")
+    public
+    @ResponseBody
+    PostBean newPost(@RequestParam(value = "title", required = false, defaultValue = "Default Title") String title, @RequestParam(value = "content", required = false, defaultValue = "Default Content") String content) {
+        PostBean post = new PostBean(title, content);
+        posts.add(post);
+        return post;
     }
 }
 
