193d0425b1c354af97be1f68c80a717716695b03
Skyler Layne <skylerclayne@gmail.com>
2016-06-14 08:58:37 -0400

    update gitignore

---
+++ src/main/java/me/skylerlayne/dev/main/MainController.java
@@ -0,0 +1,37 @@
+package me.skylerlayne.dev.main;
+
+
+import me.skylerlayne.dev.bean.PostBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * This class represents the main controller for the API.
+ * <p>
+ * Created by Skyler Layne © 2016
+ */
+@Controller
+public class MainController {
+
+
+
+    @RequestMapping(method= RequestMethod.GET, value="/test-route")
+    public @ResponseBody
+    PostBean newPost(@RequestParam(value="title", required=false, defaultValue="Post Title") String title, @RequestParam(value="content", required=false, defaultValue="Post Content") String content) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext(
+                "Spring-Module.xml");
+
+
+
+        PostBean obj = (PostBean) context.getBean("postBean");
+        System.out.print(obj.getTitle() + ": " + obj.getContent());
+
+        return new PostBean(title, content);
+    }
+}
